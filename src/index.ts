547d66ea27b234bf3e4b43b6b1688dad57838b36
Björn Benouarets <bjoern@benouarets.de>
2026-04-15 15:06:05 +0200

    init: Initialize project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import { AuthEntrypoint } from "./entrypoint";
+import { Env } from "./types";
 
-export default AuthEntrypoint;
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    return new Response("Not found", { status: 404 });
+  },
+};
+
 export { AuthEntrypoint };
